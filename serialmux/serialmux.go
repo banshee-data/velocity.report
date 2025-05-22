3e095a80c1c1e44b70ebf71cd434283536aa8a10
Patrick O'Doherty <p@trickod.com>
2025-05-22 00:12:12 -0700

    serialmux: init serialmux; fix gracefully shutdown
    Add serialmux abstraction over serial.Port to allow for multiple
    subscribers to monitor the serial port & issue commands at once.
    
    Add debug endpoints for live command interaction & tailing the serial
    output.
    
    Fix graceful shutdown of the various main goroutines such that CTRL-C
    now works as expected.

---
+++ serialmux/serialmux.go
@@ -0,0 +1,245 @@
+// Serialmux provides an abstraction over a serial port with the ability for
+// multiple clients to subscribe to events from the serial port and send
+// commands to a single serial port device.
+package serialmux
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	crand "crypto/rand"
+	"embed"
+	"encoding/hex"
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"slices"
+	"strings"
+	"sync"
+
+	"github.com/banshee-data/radar/radar"
+	"tailscale.com/tsweb"
+)
+
+var ErrWriteFailed = fmt.Errorf("failed to write to serial port")
+
+//go:embed templates/*
+var adminTemplateFS embed.FS
+
+var sendCommandTemplate = template.Must(template.ParseFS(adminTemplateFS, "templates/send-command.html.tmpl"))
+
+// SerialMux is a generic serial port multiplexer that allows multiple clients to
+// subscribe to events from a single serial port.
+type SerialMux[T SerialPorter] struct {
+	port         T
+	subscribers  map[string]chan string
+	subscriberMu sync.Mutex
+	commandMu    sync.Mutex
+}
+
+// SerialMuxInterface defines the interface for the SerialMux type.
+type SerialMuxInterface interface {
+	// Subscribe creates a new channel for receiving line events from the serial
+	// port. The channel ID is used to identify the unique channel when
+	// unsubscribing.
+	Subscribe() (string, chan string)
+	// Unsubscribe removes a channel from the list of subscribers.
+	Unsubscribe(string)
+	// SendCommand writes the provided command to the serial port.
+	SendCommand(string) error
+	// Monitor reads lines from the serial port and sends them to the
+	// appropriate channels.
+	Monitor(context.Context) error
+	// Close closes all subscribed channels and closes the serial port.
+	Close() error
+
+	// AttachAdminRoutes attaches admin debugging endpoints to the given HTTP
+	// mux served at /debug/. These routes are accessible only over
+	// localhost/via Tailscale and are not publicly accessible.
+	AttachAdminRoutes(*http.ServeMux)
+}
+
+// NewSerialMux creates a SerialMux instance backed by a serial port at the
+// given path.
+func NewSerialMux[T SerialPorter](port T) *SerialMux[T] {
+	return &SerialMux[T]{
+		port:         port,
+		subscribers:  make(map[string]chan string),
+		subscriberMu: sync.Mutex{},
+		commandMu:    sync.Mutex{},
+	}
+}
+
+// randomID generates a random channel ID (8 byte random hex encoded value)
+func randomID() string {
+	b := make([]byte, 8)
+	crand.Read(b)
+	return hex.EncodeToString(b)
+}
+
+func (s *SerialMux[T]) Subscribe() (string, chan string) {
+	id := randomID()
+	ch := make(chan string)
+	s.subscriberMu.Lock()
+	defer s.subscriberMu.Unlock()
+	s.subscribers[id] = ch
+	return id, ch
+}
+
+// Unsubscribe removes a subscriber from the serial mux.
+func (s *SerialMux[T]) Unsubscribe(id string) {
+	s.subscriberMu.Lock()
+	defer s.subscriberMu.Unlock()
+	delete(s.subscribers, id)
+}
+
+// SendCommand sends a command to the serial port.
+func (s *SerialMux[T]) SendCommand(command string) error {
+	s.commandMu.Lock()
+	defer s.commandMu.Unlock()
+	n, err := s.port.Write([]byte(command))
+	if err != nil {
+		return err
+	}
+	if n != len(command) {
+		return ErrWriteFailed
+	}
+	return nil
+}
+
+// Monitor monitors the serial port for events and sends them to subscribers
+func (s *SerialMux[T]) Monitor(ctx context.Context) error {
+	scan := bufio.NewScanner(s.port)
+
+	lineChan := make(chan string)
+	// start a goroutine to read from the serial port & send any lines that are scanned to linesChan.
+	// and any errors to the errChan
+	//
+	// the blocking scan.Scan will not interfere with our outer loop awaiting
+	// lines & context cancellation.
+	go func() {
+		defer close(lineChan)
+		for scan.Scan() {
+			select {
+			case lineChan <- scan.Text():
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	for {
+		select {
+		// check if the context is done
+		// and exit the loop if so
+		case <-ctx.Done():
+			return ctx.Err()
+
+		case line, ok := <-lineChan:
+			// if the channel is closed, we're done reading from the serial port
+			// so we should return any final scanner error
+			if !ok {
+				return scan.Err()
+			}
+			// otherwise take a read lock on the subscriber map
+			s.subscriberMu.Lock()
+			for _, ch := range s.subscribers {
+				select {
+				case ch <- line:
+				default:
+					// if the channel is full/blocking skip so as not to block the outer loop
+				}
+			}
+			s.subscriberMu.Unlock()
+		}
+	}
+}
+
+func (s *SerialMux[T]) Close() error {
+	s.subscriberMu.Lock()
+	defer s.subscriberMu.Unlock()
+	for _, ch := range s.subscribers {
+		close(ch)
+	}
+	s.subscribers = make(map[string]chan string)
+	return s.port.Close()
+}
+
+func (s *SerialMux[T]) AttachAdminRoutes(mux *http.ServeMux) {
+	debug := tsweb.Debugger(mux)
+
+	// Basic command / live tail monitor interface using the below two API endpoints.
+	debug.HandleFunc("send-command", "send a command to the serial port", func(w http.ResponseWriter, r *http.Request) {
+		buf := bytes.NewBuffer(nil)
+		if err := sendCommandTemplate.Execute(buf, nil); err != nil {
+			http.Error(w, "Failed to render template", http.StatusInternalServerError)
+			return
+		}
+		io.Copy(w, buf)
+	})
+
+	// API endpoint to write command to the serial port
+	debug.HandleSilentFunc("send-command-api", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		command := strings.TrimSpace(r.FormValue("command"))
+		if command == "" {
+			http.Error(w, "Missing command", http.StatusBadRequest)
+			return
+		}
+		if !slices.Contains(radar.AllowedCommands, command) {
+			http.Error(w, "Invalid command", http.StatusBadRequest)
+			return
+		}
+		if err := s.SendCommand(command); err != nil {
+			http.Error(w, "Failed to write command", http.StatusInternalServerError)
+			return
+		}
+		io.WriteString(w, fmt.Sprintf("Wrote command %q to serial port", command))
+	})
+	// API endpoint to issue Server-Side Events (SSE) in response to lines coming from the serial port.
+	debug.HandleSilentFunc("tail", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+
+		id, c := s.Subscribe()
+		defer s.Unsubscribe(id)
+		for {
+			select {
+			case payload := <-c:
+				_, err := w.Write([]byte(fmt.Sprintf("%s\n\n", payload)))
+				if err != nil {
+					http.Error(w, "Failed to write event", http.StatusInternalServerError)
+					return
+				}
+				w.(http.Flusher).Flush()
+			case <-r.Context().Done():
+				return
+			}
+		}
+	})
+
+	debug.HandleSilentFunc("tail.js", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/javascript")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+
+		// serve tail.js from adminTemplateFS
+		f, err := adminTemplateFS.Open("templates/tail.js")
+		if err != nil {
+			http.Error(w, "Failed to open tail.js", http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+		io.Copy(w, f)
+	})
+}
