8a1a4f92d004d0c0bdac2b7403c6adb930def077
David Dolphin <445312+ddol@users.noreply.github.com>
2025-08-13 15:20:30 -0700

    [js] tailwind.config, JSDoc + cjs

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,7 +1,10 @@
-module.exports = {
+/** @type {import('tailwindcss').Config}*/
+const config = {
 	content: [
 		'./src/**/*.{html,svelte,js}',
 		'./node_modules/svelte-ux/**/*.{svelte,js}',
 		'./node_modules/layerchart/**/*.{svelte,js}'
 	]
 };
+
+module.exports = config;
