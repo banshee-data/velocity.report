000befa15099dd202deb83d20d054526a5ab0954
David Dolphin <445312+ddol@users.noreply.github.com>
2025-08-13 12:28:36 -0700

    [js] add NavMenu

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script>
 	import '../app.css';
-	import { AppLayout } from 'svelte-ux';
+	import { AppLayout, AppBar, ThemeInit, ThemeSelect } from 'svelte-ux';
 	import favicon from '$lib/assets/favicon.svg';
+	import NavMenu from './_NavMenu.svelte';
 
 	/** @type {{ children?: import('svelte').Snippet }} */
 	let { children } = $props();
@@ -11,6 +12,20 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<ThemeInit />
+
 <AppLayout>
-	{@render children?.()}
+	{#snippet nav()}
+		<NavMenu />
+	{/snippet}
+
+	<AppBar title="Velocity Report">
+		<div class="absolute left-0 top-0 z-10 w-full px-2 py-1 text-right">
+			<ThemeSelect />
+		</div>
+	</AppBar>
+
+	<div class="p-6">
+		{@render children?.()}
+	</div>
 </AppLayout>
---
+++ web/src/routes/_NavMenu.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { NavItem } from 'svelte-ux';
+	import { page } from '$app/state';
+</script>
+
+<NavItem text="Dashboard" path="/" currentUrl={page.url} />
+<NavItem text="Settings" path="/settings" currentUrl={page.url} />
