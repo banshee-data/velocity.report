b9280c198c221e59b6577a859b5de12d2bf39559
David Dolphin <445312+ddol@users.noreply.github.com>
2025-08-13 11:49:14 -0700

    [js] npm i layerstack 2.0.0-next, tailwind 4

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: [
-    './src/**/*.{html,svelte}',
+    './src/**/*.{html,svelte,js}',
     './node_modules/svelte-ux/**/*.{svelte,js}',
     './node_modules/layerchart/**/*.{svelte,js}'
   ],
