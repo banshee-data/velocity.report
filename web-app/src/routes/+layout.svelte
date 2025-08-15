f5a789a6c25b3f58bae583026ee5d3b3ac1a88c2
David Dolphin <445312+ddol@users.noreply.github.com>
2025-08-14 21:40:03 -0700

    [js] add svelte-ux app shell

--- web-app/src/routes/+layout.svelte
+++ web-app/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { AppLayout, ThemeInit, AppBar, ThemeSelect } from 'svelte-ux';
 
 	let { children } = $props();
 </script>
@@ -9,4 +10,21 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<ThemeInit />
+
+<AppLayout>
+	<svelte:fragment slot="nav">
+		<!-- Nav menu -->
+	</svelte:fragment>
+
+	<AppBar title="Example">
+		<div slot="actions">
+			<!-- App actions -->
+		</div>
+		<ThemeSelect />
+	</AppBar>
+
+	<main>
+		{@render children?.()}
+	</main>
+</AppLayout>
