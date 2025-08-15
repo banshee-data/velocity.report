876c2f6e50c74d50146c3bb74fac148a57343724
David Dolphin <445312+ddol@users.noreply.github.com>
2025-08-14 21:39:47 -0700

    [js] add layerstack plugin to tailwind config

--- web-app/tailwind.config.js
+++ web-app/tailwind.config.js
@@ -1,8 +1,13 @@
+import layerstack from '@layerstack/tailwind/plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/svelte-ux/**/*.{svelte,js}'
+	],
 	theme: {
 		extend: {}
 	},
-	plugins: []
+
+	plugins: [layerstack]
 };
