861c940957229dfbd0c1181334374d59064b9a86
David Dolphin <445312+ddol@users.noreply.github.com>
2025-08-21 15:10:10 -0700

    [js] add proxy to go API server

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		proxy: {
+			'/api': 'http://localhost:8080'
+		}
+	},
 	plugins: [tailwindcss(), sveltekit()],
 	optimizeDeps: {
 		exclude: [
