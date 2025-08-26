7e545eea7c8a4f622fade30edd38d87f6b5ac76d
David Dolphin <445312+ddol@users.noreply.github.com>
2025-08-26 12:26:59 -0700

    [js] add Header and <main> to settings

--- web/src/routes/settings/+page.svelte
+++ web/src/routes/settings/+page.svelte
@@ -1,12 +1,13 @@
 <script>
-	import { Card } from 'svelte-ux';
+	import { Card, Header } from 'svelte-ux';
 </script>
 
 <svelte:head>
 	<title>Settings 🚴 velocity.report</title>
 </svelte:head>
 
-<Card>
-	<h2 class="text-lg font-bold">Settings</h2>
-	<p class="text-muted text-sm">Manage settings and preferences.</p>
-</Card>
+<main>
+	<Card>
+		<Header title="Settings" subheading="Manage your application settings and preferences." />
+	</Card>
+</main>
