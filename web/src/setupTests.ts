c9dba2b90226d15f7fe302b60d939115c16820c2
David Dolphin <445312+ddol@users.noreply.github.com>
2025-10-23 00:08:21 -0700

    [js] lint format

--- web/src/setupTests.ts
+++ web/src/setupTests.ts
@@ -1,35 +1,37 @@
 import '@testing-library/jest-dom';
 
 // Mock window.location
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 delete (window as any).location;
 window.location = {
-  origin: 'http://localhost:3000',
-  href: 'http://localhost:3000',
-  pathname: '/',
-  search: '',
-  hash: ''
+	origin: 'http://localhost:3000',
+	href: 'http://localhost:3000',
+	pathname: '/',
+	search: '',
+	hash: ''
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 } as any;
 
 // Mock localStorage
 const localStorageMock = (() => {
-  let store: Record<string, string> = {};
+	let store: Record<string, string> = {};
 
-  return {
-    getItem: (key: string) => store[key] || null,
-    setItem: (key: string, value: string) => {
-      store[key] = value.toString();
-    },
-    removeItem: (key: string) => {
-      delete store[key];
-    },
-    clear: () => {
-      store = {};
-    }
-  };
+	return {
+		getItem: (key: string) => store[key] || null,
+		setItem: (key: string, value: string) => {
+			store[key] = value.toString();
+		},
+		removeItem: (key: string) => {
+			delete store[key];
+		},
+		clear: () => {
+			store = {};
+		}
+	};
 })();
 
 Object.defineProperty(window, 'localStorage', {
-  value: localStorageMock
+	value: localStorageMock
 });
 
 // Reset localStorage before each test
