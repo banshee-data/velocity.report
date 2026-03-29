695fdc014cb3b6d7686f97958ba36638aa9dca06
David Dolphin <445312+ddol@users.noreply.github.com>
2026-03-29 13:01:57 -0700

    [img] update velocity-log alias to include nginx service for comprehensive logging

--- image/stage-velocity/03-velocity-config/files/velocity-aliases.sh
+++ image/stage-velocity/03-velocity-config/files/velocity-aliases.sh
@@ -1,7 +1,7 @@
 # velocity.report service management aliases
 # Installed to /etc/profile.d/ for all interactive shells.
 
-alias velocity-log='journalctl -u velocity-report.service -f --no-hostname -o short | sed "s/^[^[]*\[/[/"'
+alias velocity-log='journalctl -u velocity-report.service -u nginx.service -f --no-hostname -o short | sed "s/^[^[]*\[/[/"'
 alias velocity-status='systemctl status velocity-report.service'
 alias velocity-bounce='sudo systemctl restart velocity-report.service'
 alias velocity-stop='sudo systemctl stop velocity-report.service'
